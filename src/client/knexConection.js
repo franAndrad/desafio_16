0eec79f515e5749537fe5b8b6900e01ee0770105
franAndrad <franciscoezequielandrade@gmail.com>
2022-12-20 23:21:52 -0300

    first commit

---
+++ src/client/knexConection.js
@@ -0,0 +1,10 @@
+import knex from "knex"
+
+const knexConection = knex({
+    client: "sqlite3",
+    connection: {
+        filename: './DB/ecommerce.sqlite'
+    }
+})
+
+export default knexConection
